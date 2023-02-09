c28d018b2b3e543b5941d79d4bc9969235de2559
Fatih Arapoglu <f3tt4n@gmail.com>
2023-02-10 01:45:52 +0300

    install firebase

---
+++ src/components/m-header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div>logo</div>
+            <div>links?</div>
+        </header>
+    );
+};
+
+export default Header;
