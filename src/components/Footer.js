0b4e3491949b3ba64052b4d159188c82f25b6604
Fatih Arapoglu <f3tt4n@gmail.com>
2023-02-11 12:11:33 +0300

    add footer, add scenes and components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,25 @@
 import React from "react";
+import github from "../assets/GitHub-Mark-Light-120px-plus.png";
 
 const Footer = () => {
-    return <footer>Footer</footer>;
+    return (
+        <footer className="footer">
+            <div className="about">
+                <span>
+                    <a
+                        href="https://www.theodinproject.com/lessons/node-path-javascript-where-s-waldo-a-photo-tagging-app"
+                        id="odin"
+                    >
+                        The Odin Project{" "}
+                    </a>
+                    | fatiharapoglu
+                </span>
+                <a href="https://github.com/fatiharapoglu">
+                    <img className="github-icon" src={github} alt="github-logo" />
+                </a>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import ps1 from "../assets/pierre-roussel-ps1-phone2.jpg";
+import ps2 from "../assets/pierre-roussel-ps2-phone2.jpg";
+import ps3 from "../assets/pierre-roussel-ps3-phone2.jpg";
+import ps4 from "../assets/pierre-roussel-ps4-phone2.jpg";
 
 const Main = () => {
-    return <main>Main</main>;
+    return (
+        <main className="main">
+            <h1>PICK A SCENE</h1>
+            <div className="scenes">
+                <Link to="/game">
+                    <img src={ps1} alt="ps1" />
+                </Link>
+                <Link to="/game">
+                    <img src={ps2} alt="ps2" />
+                </Link>
+                <Link to="/game">
+                    <img src={ps3} alt="ps3" />
+                </Link>
+                <Link to="/game">
+                    <img src={ps4} alt="ps4" />
+                </Link>
+            </div>
+        </main>
+    );
 };
 
 export default Main;
